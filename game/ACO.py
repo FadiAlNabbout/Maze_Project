d0d2b955100853986c60703b2e7bc078b5a909ff
Liamsi4657 <jeq.ismail@gmail.com>
2023-05-24 16:57:36 +0200

    ACO complete

--- game/ACO.py
+++ game/ACO.py
@@ -1,123 +1,121 @@
 import random
-import numpy as np
-from maze_generation import generate_maze
+
 
 class Ant:
-    def __init__(self, maze, aco):
+    def __init__(self, maze, alpha, beta, rho):
         self.maze = maze
+        self.alpha = alpha  # pheromone importance
+        self.beta = beta  # heuristic information importance
+        self.rho = rho  # pheromone evaporation rate
+        self.num_rows = len(maze)
+        self.num_cols = len(maze[0])
         self.path = []
-        self.aco = aco
-        self.current_position = self.get_start_position()
-    def get_start_position(self):
-        start_positions = np.where(self.maze == 2)
-        return tuple((start_positions[0][0], start_positions[1][0]))
+        self.visited = set()
+        self.current_position = None
+        self.goal_reached = False
+        self.start = self.find_start()
 
-    def get_possible_moves(self, position):
-        row, col = position
+    def find_start(self):
+        for i in range(self.num_rows):
+            for j in range(self.num_cols):
+                if self.maze[i][j] == 2:
+                    return (i, j)
+
+    def select_next_position(self):
+        row, col = self.current_position
         possible_moves = []
 
-        # Check the four adjacent positions: up, down, left, right
-        # Add valid moves to the list of possible moves
-        if row - 1 >= 0 and self.maze[row - 1][col] != 0:
-            possible_moves.append((row - 1, col))
-        if row + 1 < len(self.maze) and self.maze[row + 1][col] != 0:
-            possible_moves.append((row + 1, col))
-        if col - 1 >= 0 and self.maze[row][col - 1] != 0:
-            possible_moves.append((row, col - 1))
-        if col + 1 < len(self.maze[row]) and self.maze[row][col + 1] != 0:
-            possible_moves.append((row, col + 1))
+        # Check neighboring cells
+        for drow, dcol in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            next_row, next_col = row + drow, col + dcol
+            if 0 <= next_row < self.num_rows and 0 <= next_col < self.num_cols:
+                cell_type = self.maze[next_row][next_col]
 
-        return possible_moves
+                if cell_type == 0 or (next_row, next_col) in self.visited:
+                    continue  # Ignore walls and previously visited cells
 
-    def move(self):
-        self.path.append(self.current_position)  # Initialize the path with the starting position
-        while not self.maze[self.current_position[0]][self.current_position[1]] == 3:
-            possible_moves = self.get_possible_moves(self.current_position)
+                if cell_type == 4:
+                    possible_moves.append((next_row, next_col, 3))  # Rough terrain preference
+                elif cell_type == 5:
+                    possible_moves.append((next_row, next_col, 1))  # Water terrain preference
+                else:
+                    possible_moves.append((next_row, next_col, 2))  # Normal path preference
 
-            attractiveness_values = [
-                self.aco.get_attractiveness(move) for move in possible_moves
-            ]
-            total_attractiveness = sum(attractiveness_values)
-            probabilities = [
-                attractiveness / total_attractiveness for attractiveness in attractiveness_values
-            ]
+        if not possible_moves:
+            return  # No valid moves available
 
-            chosen_move = random.choices(possible_moves, probabilities)[0]
+        # Calculate probabilities based on preferences and pheromone levels
+        probabilities = []
+        total_pheromone = sum(self.maze[row][col] for row, col, _ in possible_moves)
 
-            self.current_position = chosen_move
-            self.path.append(chosen_move)
-            break
+        for move in possible_moves:
+            pheromone = self.maze[move[0]][move[1]]
+            preference = move[2]
+
+            probabilities.append((pheromone ** self.alpha) * (preference ** self.beta) / total_pheromone)
+
+        # Choose next position using ACO probabilities
+        selected_move = random.choices(possible_moves, probabilities)[0][:2]
+        self.path.append(selected_move)
+        self.current_position = selected_move
+        self.visited.add(selected_move)
+
+        if self.maze[selected_move[0]][selected_move[1]] == 3:
+            self.goal_reached = True
+
+    def update_pheromone(self, pheromone_matrix):
+        path_length = len(self.path)
+        pheromone_deposit = 1 / path_length
+
+        for position in self.path:
+            pheromone_matrix[position[0]][position[1]] += pheromone_deposit
+
+    def find_path(self):
+        print('Finding path...')
+        self.current_position = self.start
+        self.visited.add(self.start)
+        self.path.append(self.start)
+
+        while not self.goal_reached:
+            self.select_next_position()
+
+        print('Path found:', self.path)
+        return self.path
 
 
-class ACO:
-    def __init__(self, maze, num_ants, evaporation_rate, alpha, beta):
+class ACOAlgorithm:
+    def __init__(self, maze, num_ants, num_iterations, alpha, beta, rho):
         self.maze = maze
         self.num_ants = num_ants
-        self.evaporation_rate = evaporation_rate
+        self.num_iterations = num_iterations
         self.alpha = alpha
         self.beta = beta
-        self.pheromone = self.initialize_pheromone()
+        self.rho = rho
 
-    def initialize_pheromone(self):
-        # Initialize the pheromone trails in the maze with initial intensity values
-        pheromone = np.ones(self.maze.shape, dtype=float)
-        pheromone[self.maze == 0] = 0  # Set pheromone on walls (0) to 0
-        return pheromone
+    def solve(self):
+        best_path = None
+        best_path_length = float('inf')
 
-    def update_pheromone_trails(self, ants):
-        # Evaporate pheromone trails
-        self.evaporate_pheromone()
+        # Initialize pheromone matrix
+        pheromone_matrix = [[1] * len(self.maze[0]) for _ in range(len(self.maze))]
 
-        # Update pheromone trails based on ant paths
-        for ant in ants:
-            path_length = self.calculate_path_length(ant.path)
-            if path_length != 0:  # Check if path_length is not zero
-                pheromone_deposit = 1 / path_length
-                for i in range(len(ant.path) - 1):
-                    current_pos = ant.path[i]
-                    next_pos = ant.path[i + 1]
-                    self.pheromone[current_pos, next_pos] += pheromone_deposit
+        for _ in range(self.num_iterations):
+            paths = []
 
-    def evaporate_pheromone(self):
-        self.pheromone *= (1 - self.evaporation_rate)
+            for _ in range(self.num_ants):
+                ant = Ant(self.maze, self.alpha, self.beta, self.rho)
+                path = ant.find_path()
+                paths.append(path)
 
-    def get_attractiveness(self, move):
-        terrain_type = self.maze[move]
-        if terrain_type == 1:  # Normal terrain
-            return 1
-        elif terrain_type == 4:  # Rough terrain
-            return 2
-        elif terrain_type == 5:  # Water terrain
-            return 0.1
-        elif terrain_type == 3:  # goal
-            return 100
-        else:
-            return 0
+                if len(path) < best_path_length:
+                    best_path = path
+                    best_path_length = len(path)
 
-    def solve(self, num_iterations):
-        ants = [Ant(self.maze, self) for _ in range(self.num_ants)]
+                ant.update_pheromone(pheromone_matrix)
 
-        for _ in range(num_iterations):
-            for ant in ants:
-                ant.move()
-
-            self.update_pheromone_trails(ants)
-            self.evaporate_pheromone()
-
-        best_path = max(ants, key=lambda ant: self.calculate_path_length(ant.path)).path
+            # Update pheromone matrix by evaporation
+            for i in range(len(pheromone_matrix)):
+                for j in range(len(pheromone_matrix[0])):
+                    pheromone_matrix[i][j] *= (1 - self.rho)
 
         return best_path
-
-    def calculate_path_length(self, path):
-        # Calculate the length of a given path in the maze
-        return len(path)
-
-
-maze = generate_maze(4, 4)
-print(maze)
-
-aco = ACO(maze, num_ants=1, alpha=1, beta=2, evaporation_rate=0.5)
-best_path = aco.solve(num_iterations=10)
-
-print("Best Path:", best_path)
-print("Best Path Length:", len(best_path))
